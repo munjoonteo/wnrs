0b2748ae07374ddbb34cc5151a40271eac57598a
munjoonteo <teomunjoon101@gmail.com>
2020-11-25 21:44:53 +1100

    added functionality for buttons

--- src/App.js
+++ src/App.js
@@ -3,49 +3,59 @@ import Card from "./Card";
 import "./App.css";
 
 function App() {
-  const [currLevel, setLevel] = React.useState("one");
-  const levels = ["one", "two", "three"];
-  const buttons = levels.map(level => (
+  const levels = {
+    levelOne: ["q1", "hi"],
+    levelTwo: ["q2", "sad"],
+    levelThree: ["q3"],
+  };
+
+  const [currLevel, setLevel] = React.useState(Object.keys(levels)[0]);
+  const [currCard, setCurrCard] = React.useState(levels[currLevel].shift());
+  const [cardHistory, setCardHistory] = React.useState([]);
+
+  const buttons = Object.keys(levels).map(level => (
     <button
       className={`level-button ${level === currLevel ? "selected-level" : ""}`}
-      onClick={() => setLevel(level)}
+      onClick={() => handleChangeLevel(level)}
     >
-      level {level}
+      {level.split(/(?=[A-Z])/).join(" ")}
     </button>
   ));
 
-  // const bigCardClasses = classNames({
-  //   "big-card": true,
-  //   card: true,
-  // });
+  function handleChangeLevel(newLevel) {
+    setLevel(newLevel);
+    setCurrCard(levels[newLevel].shift());
+  }
+
+  function handleNextCard() {
+    if (levels[currLevel].length === 0) {
+      setCurrCard("You have finished this level!");
+    } else {
+      const tempHistory = [currCard, ...cardHistory];
+      setCardHistory(tempHistory);
+      setCurrCard(levels[currLevel].shift());
+    }
+    console.log(currCard);
+    console.log(levels[currLevel].shift());
+  }
 
   return (
     <div className="App">
       <div className="levels">{buttons}</div>
       <div className="question">
         <div className="title">we're not really strangers</div>
-        <Card styleName="big-card" />
-        <button className="next-card-button">next card</button>
+        <Card styleName="big-card" question={currCard} />
+        <button className="next-card-button" onClick={() => handleNextCard()}>
+          next card
+        </button>
       </div>
       <div className="history">
         <div className="history-title">previous cards</div>
         <div className="card-container">
           <div className="card-container-scroll">
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
-            <Card styleName="small-card" />
+            {cardHistory.map(qn => (
+              <Card styleName="small-card" question={qn} />
+            ))}
           </div>
         </div>
       </div>
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 
 function Card(props) {
-  return <div className={`card ${props.styleName}`}>hi</div>;
+  return <div className={`card ${props.styleName}`}>{props.question}</div>;
 }
 
 export default Card;
